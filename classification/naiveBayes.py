f0368f704deac9cba05dba6f8e96f479c3b66bda
nprez <nick_prezioso@hotmail.com>
2018-05-04 23:22:34 -0400

    more indentation

--- classification/naiveBayes.py
+++ classification/naiveBayes.py
@@ -11,205 +11,205 @@ import classificationMethod
 import math
 
 class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
-  """
-  See the project description for the specifications of the Naive Bayes classifier.
-  
-  Note that the variable 'datum' in this code refers to a counter of features
-  (not to a raw samples.Datum).
-  """
-  def __init__(self, legalLabels):
-    self.legalLabels = legalLabels
-    self.type = "naivebayes"
-    self.k = 1 # this is the smoothing parameter, ** use it in your train method **
-    self.automaticTuning = False # Look at this flag to decide whether to choose k automatically ** use this in your train method **
+    """
+    See the project description for the specifications of the Naive Bayes classifier.
     
-  def setSmoothing(self, k):
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
     """
-    This is used by the main method to change the smoothing parameter before training.
-    Do not modify this method.
-    """
-    self.k = k
+    def __init__(self, legalLabels):
+        self.legalLabels = legalLabels
+        self.type = "naivebayes"
+        self.k = 1 # this is the smoothing parameter, ** use it in your train method **
+        self.automaticTuning = False # Look at this flag to decide whether to choose k automatically ** use this in your train method **
 
-  def train(self, trainingData, trainingLabels, validationData, validationLabels):
-    """
-    Outside shell to call your method. Do not modify this method.
-    """  
-      
-    # might be useful in your code later...
-    # this is a list of all features in the training set.
-    self.features = list(set([ f for datum in trainingData for f in datum.keys() ]));
-    
-    if (self.automaticTuning):
-        kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
-    else:
-        kgrid = [self.k]
+    def setSmoothing(self, k):
+        """
+        This is used by the main method to change the smoothing parameter before training.
+        Do not modify this method.
+        """
+        self.k = k
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        """
+        Outside shell to call your method. Do not modify this method.
+        """  
+          
+        # might be useful in your code later...
+        # this is a list of all features in the training set.
+        self.features = list(set([ f for datum in trainingData for f in datum.keys() ]));
         
-    self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
-      
-  def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
-    """
-    Trains the classifier by collecting counts over the training data, and
-    stores the Laplace smoothed estimates so that they can be used to classify.
-    Evaluate each value of k in kgrid to choose the smoothing parameter 
-    that gives the best accuracy on the held-out validationData.
-    
-    trainingData and validationData are lists of feature Counters.  The corresponding
-    label lists contain the correct label for each datum.
-    
-    To get the list of all possible features or labels, use self.features and 
-    self.legalLabels.
-    """
+        if (self.automaticTuning):
+            kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
+        else:
+            kgrid = [self.k]
+            
+        self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
 
-    "*** YOUR CODE HERE ***"
+    def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
+        """
+        Trains the classifier by collecting counts over the training data, and
+        stores the Laplace smoothed estimates so that they can be used to classify.
+        Evaluate each value of k in kgrid to choose the smoothing parameter 
+        that gives the best accuracy on the held-out validationData.
+        
+        trainingData and validationData are lists of feature Counters.  The corresponding
+        label lists contain the correct label for each datum.
+        
+        To get the list of all possible features or labels, use self.features and 
+        self.legalLabels.
+        """ 
 
-    kpList = []
+        "*** YOUR CODE HERE ***"    
 
-    for k in kgrid:
-        blankDic = {}
+        kpList = [] 
+
+        for k in kgrid:
+            blankDic = {}
+            for label in self.legalLabels:
+                blankDic[label] = {}
+                for feature in self.features:
+                    blankDic[label][feature] = 0
+            kpList.append(blankDic) 
+
+        numLabel = {}
         for label in self.legalLabels:
-            blankDic[label] = {}
-            for feature in self.features:
-                blankDic[label][feature] = 0
-        kpList.append(blankDic)
+            numLabel[label] = 0 
 
-    numLabel = {}
-    for label in self.legalLabels:
-        numLabel[label] = 0
-
-    for k in range(len(kpList)):
-        for datum in range(len(trainingData)):
-            label = trainingLabels[datum]
-            if(k==0):
-                numLabel[label]+=1
-            for f in trainingData[datum]:
-                kpList[k][label][f] += trainingData[datum][f]
-    
-    for k in range(len(kpList)):
-        for label in kpList[k]:
-            for feature in kpList[k][label]:
-                kpList[k][label][feature] = float(kpList[k][label][feature]+kgrid[k])/float(numLabel[label]+(2*kgrid[k]))
-
-    valList = {}
-    for label in self.legalLabels:
-        valList[label] = {}
-        for feature in kpList[0][label]:
-            valList[label][feature] = 0
-
-    valNumLabel = {}
-    for label in self.legalLabels:
-        valNumLabel[label] = 0
-
-    for datum in range(len(validationData)):
-        label = validationLabels[datum]
-        valNumLabel[label]+=1
-        for f in validationData[datum]:
-            valList[label][f] += validationData[datum][f]
-
-    for label in valList:
-        for feature in valList[label]:
-            valList[label][feature] = float(valList[label][feature])/float(valNumLabel[label])
-
-    diffList = []
-    for k in range(len(kpList)):
-        totalDiff = 0
-        for label in kpList[k]:
-            for feature in kpList[k][label]:
-                totalDiff+=kpList[k][label][feature]-valList[label][feature]
-        diffList.append(totalDiff)
-
-    min = 0
-    for k in range(len(diffList)):
-        if(abs(diffList[k])<abs(diffList[min])):
-            min = k
-
-    #set later
-    self.probabilities = kpList[min]
-    self.labelProbabilities = {}
-    for label in self.legalLabels:
-        self.labelProbabilities[label] = float(numLabel[label])/float(len(trainingData))
-
-    print "label probabilities:"
-    print self.labelProbabilities
-    print "best k: "+str(kgrid[min])
-
-    #util.raiseNotDefined()
+        for k in range(len(kpList)):
+            for datum in range(len(trainingData)):
+                label = trainingLabels[datum]
+                if(k==0):
+                    numLabel[label]+=1
+                for f in trainingData[datum]:
+                    kpList[k][label][f] += trainingData[datum][f]
         
-  def classify(self, testData):
-    """
-    Classify the data based on the posterior distribution over labels.
-    
-    You shouldn't modify this method.
-    """
-    guesses = []
-    self.posteriors = [] # Log posteriors are stored for later data analysis (autograder).
-    for datum in testData:
-      posterior = self.calculateLogJointProbabilities(datum)
-      guesses.append(posterior.argMax())
-      self.posteriors.append(posterior)
-    return guesses
-      
-  def calculateLogJointProbabilities(self, datum):
-    """
-    Returns the log-joint distribution over legal labels and the datum.
-    Each log-probability should be stored in the log-joint counter, e.g.    
-    logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
-    
-    To get the list of all possible features or labels, use self.features and 
-    self.legalLabels.
-    """
+        for k in range(len(kpList)):
+            for label in kpList[k]:
+                for feature in kpList[k][label]:
+                    kpList[k][label][feature] = float(kpList[k][label][feature]+kgrid[k])/float(numLabel[label]+(2*kgrid[k]))   
 
-    logJoint = util.Counter()
-    
-    "*** YOUR CODE HERE ***"
-    """
-    pY = util.Counter 
+        valList = {}
+        for label in self.legalLabels:
+            valList[label] = {}
+            for feature in kpList[0][label]:
+                valList[label][feature] = 0 
 
-    for label in self.legalLabels:
-        logJoint[label] = math.log(self.labelProbabilities[label])
-        for feat, value in datum.items
-            if value > 0:
-                logJoint[label] += math.log(self.probabilities[feat,label])
-            else:
-                logJoint[label] += math.log(1-self.probabilities[feat,label])
+        valNumLabel = {}
+        for label in self.legalLabels:
+            valNumLabel[label] = 0  
+
+        for datum in range(len(validationData)):
+            label = validationLabels[datum]
+            valNumLabel[label]+=1
+            for f in validationData[datum]:
+                valList[label][f] += validationData[datum][f]   
+
+        for label in valList:
+            for feature in valList[label]:
+                valList[label][feature] = float(valList[label][feature])/float(valNumLabel[label])  
+
+        diffList = []
+        for k in range(len(kpList)):
+            totalDiff = 0
+            for label in kpList[k]:
+                for feature in kpList[k][label]:
+                    totalDiff+=kpList[k][label][feature]-valList[label][feature]
+            diffList.append(totalDiff)  
+
+        min = 0
+        for k in range(len(diffList)):
+            if(abs(diffList[k])<abs(diffList[min])):
+                min = k 
+
+        #set later
+        self.probabilities = kpList[min]
+        self.labelProbabilities = {}
+        for label in self.legalLabels:
+            self.labelProbabilities[label] = float(numLabel[label])/float(len(trainingData))    
+
+        print "label probabilities:"
+        print self.labelProbabilities
+        print "best k: "+str(kgrid[min])    
+
+        #util.raiseNotDefined()
+
+    def classify(self, testData):
+        """
+        Classify the data based on the posterior distribution over labels.
+        
+        You shouldn't modify this method.
+        """
+        guesses = []
+        self.posteriors = [] # Log posteriors are stored for later data analysis (autograder).
+        for datum in testData:
+          posterior = self.calculateLogJointProbabilities(datum)
+          guesses.append(posterior.argMax())
+          self.posteriors.append(posterior)
+        return guesses
+ 
+    def calculateLogJointProbabilities(self, datum):
+        """
+        Returns the log-joint distribution over legal labels and the datum.
+        Each log-probability should be stored in the log-joint counter, e.g.    
+        logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
+        
+        To get the list of all possible features or labels, use self.features and 
+        self.legalLabels.
+        """ 
+
+        logJoint = util.Counter()
+        
+        "*** YOUR CODE HERE ***"
+        """
+        pY = util.Counter   
+
+        for label in self.legalLabels:
+            logJoint[label] = math.log(self.labelProbabilities[label])
+            for feat, value in datum.items
+                if value > 0:
+                    logJoint[label] += math.log(self.probabilities[feat,label])
+                else:
+                    logJoint[label] += math.log(1-self.probabilities[feat,label])   
+
+            return logJoint
+        
+        return logJoint
+        """ 
+
+        for label in self.legalLabels:
+            logJoint[label] = math.log(self.labelProbabilities[label])
+            for feat in datum.items():
+                if feat[1] > 0:
+                    logJoint[label] += math.log(self.probabilities[label][feat[0]])
+                else:
+                    logJoint[label] += math.log(1-self.probabilities[label][feat[0]])   
 
         return logJoint
-    
-    return logJoint
-    """
 
-    for label in self.legalLabels:
-        logJoint[label] = math.log(self.labelProbabilities[label])
-        for feat in datum.items():
-            if feat[1] > 0:
-                logJoint[label] += math.log(self.probabilities[label][feat[0]])
-            else:
-                logJoint[label] += math.log(1-self.probabilities[label][feat[0]])
+    def findHighOddsFeatures(self, label1, label2):
+        """
+        Returns the 100 best features for the odds ratio:
+                P(feature=1 | label1)/P(feature=1 | label2) 
+        
+        Note: you may find 'self.features' a useful way to loop through all possible features
+        """ 
 
-    return logJoint
+        def sortFunc(elem):
+            return elem[0]  
 
-  def findHighOddsFeatures(self, label1, label2):
-    """
-    Returns the 100 best features for the odds ratio:
-            P(feature=1 | label1)/P(feature=1 | label2) 
-    
-    Note: you may find 'self.features' a useful way to loop through all possible features
-    """
+        featuresOdds = []
+           
+        for feat in self.features:
+            featuresOdds.append((float(self.probabilities[label1][feat])/float(self.probabilities[label2][feat]), feat))
+           
+        featuresOdds.sort(key=sortFunc, reverse=True)   
 
-    def sortFunc(elem):
-        return elem[0]
+        tempList = []   
 
-    featuresOdds = []
-       
-    for feat in self.features:
-        featuresOdds.append((float(self.probabilities[label1][feat])/float(self.probabilities[label2][feat]), feat))
-       
-    featuresOdds.sort(key=sortFunc, reverse=True)
+        for i in range(100):
+            tempList.append(featuresOdds[i])    
 
-    tempList = []
+        featuresOdds = list(tempList)   
 
-    for i in range(100):
-        tempList.append(featuresOdds[i])
-
-    featuresOdds = list(tempList)
-
-    return featuresOdds
+        return featuresOdds
--- classification/perceptron.py
+++ classification/perceptron.py
@@ -11,77 +11,76 @@ import util
 PRINT = True
 
 class PerceptronClassifier:
-  """
-  Perceptron classifier.
-  
-  Note that the variable 'datum' in this code refers to a counter of features
-  (not to a raw samples.Datum).
-  """
-  def __init__( self, legalLabels, max_iterations):
-    self.legalLabels = legalLabels
-    self.type = "perceptron"
-    self.max_iterations = max_iterations
-    self.weights = {}
-    for label in legalLabels:
-        self.weights[label] = util.Counter() # this is the data-structure you should use
+    """
+    Perceptron classifier.
+    
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
+    def __init__( self, legalLabels, max_iterations):
+        self.legalLabels = legalLabels
+        self.type = "perceptron"
+        self.max_iterations = max_iterations
+        self.weights = {}
+        for label in legalLabels:
+            self.weights[label] = util.Counter() # this is the data-structure you should use
 
-  def setWeights(self, weights):
-    assert len(weights) == len(self.legalLabels);
-    self.weights == weights;
+    def setWeights(self, weights):
+        assert len(weights) == len(self.legalLabels);
+        self.weights == weights;
       
-  def train( self, trainingData, trainingLabels, validationData, validationLabels ):
-    """
-    The training loop for the perceptron passes through the training data several
-    times and updates the weight vector for each label based on classification errors.
-    See the project description for details. 
-    
-    Use the provided self.weights[label] data structure so that 
-    the classify method works correctly. Also, recall that a
-    datum is a counter from features to values for those features
-    (and thus represents a vector a values).
-    """
-    
-    self.features = trainingData[0].keys() # could be useful later
-    # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
-    # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-    
-    for iteration in range(self.max_iterations):
-        print "Starting iteration ", iteration, "..."
-        for i in range(len(trainingData)):
-            "*** YOUR CODE HERE ***"
-            trueValue = trainingData[i]
-            guessValue = util.Counter()
-            for label in self.legalLabels:
-                guessValue[label] = trueValue * self.weights[label]
-            maxValue = guessValue.argMax()
+    def train( self, trainingData, trainingLabels, validationData, validationLabels ):
+        """
+        The training loop for the perceptron passes through the training data several
+        times and updates the weight vector for each label based on classification errors.
+        See the project description for details. 
+        
+        Use the provided self.weights[label] data structure so that 
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        (and thus represents a vector a values).
+        """
+        
+        self.features = trainingData[0].keys() # could be useful later
+        # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
+        # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
+        
+        for iteration in range(self.max_iterations):
+            print "Starting iteration ", iteration, "..."
+            for i in range(len(trainingData)):
+                "*** YOUR CODE HERE ***"
+                trueValue = trainingData[i]
+                guessValue = util.Counter()
+                for label in self.legalLabels:
+                    guessValue[label] = trueValue * self.weights[label]
+                maxValue = guessValue.argMax()  
 
-            actualValue = trainingLabels[i]
-            if maxValue != actualValue:
-                self.weights[actualValue] += trueValue
-                self.weights[maxValue] -= trueValue
+                actualValue = trainingLabels[i]
+                if maxValue != actualValue:
+                    self.weights[actualValue] += trueValue
+                    self.weights[maxValue] -= trueValue
     
-  def classify(self, data):
-    """
-    Classifies each datum as the label that most closely matches the prototype vector
-    for that label.  See the project description for details.
-    
-    Recall that a datum is a util.counter... 
-    """
-    guesses = []
-    for datum in data:
-        vectors = util.Counter()
-        for l in self.legalLabels:
-            vectors[l] = self.weights[l] * datum
-        guesses.append(vectors.argMax())
-    return guesses
+    def classify(self, data):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label.  See the project description for details.
+        
+        Recall that a datum is a util.counter... 
+        """
+        guesses = []
+        for datum in data:
+            vectors = util.Counter()
+            for l in self.legalLabels:
+                vectors[l] = self.weights[l] * datum
+            guesses.append(vectors.argMax())
+        return guesses
 
-  
-  def findHighWeightFeatures(self, label):
-    """
-    Returns a list of the 100 features with the greatest weight for some label
-    """
-    featuresWeights = []
+    def findHighWeightFeatures(self, label):
+        """
+        Returns a list of the 100 features with the greatest weight for some label
+        """
+        featuresWeights = []    
 
-    "*** YOUR CODE HERE ***"
-    featuresWeights = self.weights[label].sortedKeys()[:100]
-    return featuresWeights
+        "*** YOUR CODE HERE ***"
+        featuresWeights = self.weights[label].sortedKeys()[:100]
+        return featuresWeights
